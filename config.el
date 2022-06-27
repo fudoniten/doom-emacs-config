1b2ccf9dccc6004bb94003179c35f4c1e2d0bc4b
niten <niten@fudo.org>
2022-06-27 10:07:15 -0700

    Set org & org-roam directories.

--- config.el
+++ config.el
@@ -29,7 +29,11 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(let* ((org-dir (file-truename "~/Notes"))
+       (roam-dir (format "%s/roam" org-dir)))
+  (make-directory roam-dir 'parents)
+  (setq org-directory org-dir)
+  (setq org-roam-directory roam-dir))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
