fc642ed3ae9ec5d02918e7a636bf648433599b2c
niten <niten@fudo.org>
2022-09-07 09:01:40 -0700

    See what tree-sitter is for Nix

--- init.el
+++ init.el
@@ -102,6 +102,7 @@
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
+       tree-sitter
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
@@ -144,7 +145,7 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
+       (nix +tree-sitter)  ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +journal +roam2) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
