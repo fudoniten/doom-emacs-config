52e97813f4faf1bbbcacfed2c7985c7255dd762a
peter.selby@bytedance.com <peter.selby@bytedance.com@bytedance.com>
2023-12-07 11:45:44 -0800

    FFS, missed a line while uncommenting

--- config.el
+++ config.el
@@ -157,7 +157,7 @@
         (if (or (file-regular-p full-file) (file-symlink-p full-file))
           (progn (message "Loading file %s" full-file)
                  (load full-file))
-;;           (message "Skipping invalid file %s" full-file))))))
+           (message "Skipping invalid file %s" full-file))))))
 
 (let ((local-dir (if (getenv "DOOM_EMACS_LOCAL_PATH")
                     (getenv "DOOM_EMACS_LOCAL_PATH")
