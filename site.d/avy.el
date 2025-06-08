9da5beac97ed8b580ec092cd43cb5733650ffe13
niten <niten@fudo.org>
2025-06-08 16:42:40 -0700

    feat: Add initial implementation of avy.el package

---
+++ site.d/avy.el
@@ -0,0 +1,25 @@
+;;; avy.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Niten
+;;
+;; Author: Niten <niten@fudo.org>
+;; Maintainer: Niten <niten@fudo.org>
+;; Created: June 08, 2025
+;; Modified: June 08, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/niten/avy
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'avy)
+;;; avy.el ends here
